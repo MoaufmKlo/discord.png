9476a41d902e6f5332e4f88207976e929d935e0a
MoaufmKlo <45636897+MoaufmKlo@users.noreply.github.com>
2022-03-30 18:03:14 +0200

    feat: add scale factor and data

--- index.js
+++ index.js
@@ -1,22 +1,16 @@
 /* eslint-env browser */
 const puppeteer = require('puppeteer')
 
-const browser = puppeteer.launch({
-  dumpio: true,
-  headless: false,
-  args: ['--no-sandbox', '--disable-setuid-sandbox', '--font-render-hinting none']
-})
-
 module.exports.render = async (msg, settings) => {
-  await browser
-
+  const browser = await puppeteer.launch()
   const page = await (await browser).newPage()
 
   await page.setUserAgent('Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.121 Safari/537.36')
-  await page.setViewport({ width: 1920, height: 5400 })
+  await page.setViewport({ width: 1920, height: 10800, deviceScaleFactor: 2 })
   await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:98.0) Gecko/20100101 Firefox/98.0')
 
-  await page.goto('https://discohook.org/')
+  const data = Buffer.from(JSON.stringify({ messages: [{ data: msg }] })).toString('base64')
+  await page.goto(`https://discohook.org/?data=${data}`)
 
   // set settings
   await page.evaluate((settings) => localStorage.setItem('settings', JSON.stringify(settings)), settings)
@@ -25,7 +19,13 @@ module.exports.render = async (msg, settings) => {
   // screenshot
   await page.waitForSelector('main > div > div')
   const png = await page.$('main > div > div')
+
+  await png.evaluate((element) => (element.style['padding-top'] = '1.25rem'))
+  await png.evaluate((element) => (element.style['padding-bottom'] = '1.25rem'))
+  if (!msg.bot) await page.evaluate(() => document.querySelector('main > div > div > div > span').remove())
+
   await png.screenshot({ path: 'msg.png' })
 
   await page.close()
+  await browser.close()
 }
